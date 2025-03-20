1113122e7768c45ba32c883c9ffc9d6f6b14711c
Colson <fcolson@UMICH.EDU>
2025-03-20 15:27:09 -0400

    First commit

---
+++ Assets/DebugCube.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCube : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log("Cube Position: " + transform.position);
+    }
+}
