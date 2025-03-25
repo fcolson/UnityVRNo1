f4f6b5303ec549517e3708fc1991bc881c0c1dbc
fcolson <fcolson@umich.edu>
2025-03-24 21:28:44 -0400

    work from fab studio

--- Assets/SendGrabData.cs
+++ Assets/SendGrabData.cs
@@ -1,3 +1,5 @@
+
+/*
 // using System.IO.Ports;
 using UnityEngine;
 using Oculus.Interaction;
@@ -33,5 +35,54 @@ public class SendGrabData : MonoBehaviour
             Debug.Log("grab_end");
             _isGrabbed = false;
         }
+    }
+}
+*/
+using UnityEngine;
+using Oculus.Interaction;
+
+public class SendGrabData : MonoBehaviour
+{
+    private bool _isGrabbed = false;
+
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+    private SerialGrabSender _serial;
+#endif
+
+    void Start()
+    {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        _serial = new SerialGrabSender("COM3", 9600);
+#endif
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!_isGrabbed)
+            {
+                Debug.Log("grab_start");
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+                _serial?.Send("grab_start");
+#endif
+                _isGrabbed = true;
+            }
+            else
+            {
+                Debug.Log("grab_end");
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+                _serial?.Send("grab_end");
+#endif
+                _isGrabbed = false;
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        _serial?.Close();
+#endif
     }
 }
